dafa93b4e6eeb7b6b75aee4a6537d628d7de5497
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-02-14 22:42:18 +0200

    add cards

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,16 +1,70 @@
-import { Layout } from 'antd';
+import { Card, Layout, Statistic, List, Typography } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import { FetchAssets, fakeFetchCrypto } from '../../api';
 
 const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
+  padding: '1rem',
 };
 
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
 export default function AppSider() {
+  const [loading, setLoading] = useState(false)
+  const [crypto, setCrypto] = useState([])
+  const [assets, setAssets] = useState([])
+
+  useEffect(() => {
+    async function preload() {
+      setLoading(true)
+      const { result } = await fakeFetchCrypto()
+      const assets = await FetchAssets()
+
+      setAssets(assets)
+      setCrypto(result)
+      setLoading(false)
+    }
+    preload()
+  }, [])
+
+
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-          Sider
-        </Layout.Sider>
+      <Card style={{marginBottom: '1rem'}}>
+        <Statistic 
+          title="Active"
+          value={11.28}
+          precision={2}
+          valueStyle={{color: '#3f8600'}}
+          prefix={<ArrowUpOutlined />}
+          suffix='%'
+        />
+        <List
+          size='small'
+          dataSource={data}
+          renderItem={(item) => (
+        <List.Item>
+          <Typography.Text mark>[ITEM]</Typography.Text> {item}
+        </List.Item>
+      )}
+    />
+      </Card>
+      <Card>
+        <Statistic 
+          title="Idle"
+          value={9.3}
+          precision={2}
+          valueStyle={{color: '#cf1322'}}
+          prefix={<ArrowDownOutlined />}
+          suffix='%'
+        />
+      </Card>
+    </Layout.Sider>
   )
 }
