aa35352314148daca1318ec9a1554c42ef125c68
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-03-02 23:33:23 +0200

    header (add select and button)

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button } from 'antd';
 
 const headerStyle = {
   width: '100%',
@@ -8,11 +8,60 @@ const headerStyle = {
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  background: 'white',
 };
+
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const options = [
+  {
+    label: 'China',
+    value: 'china',
+    emoji: '🇨🇳',
+    desc: 'China (中国)',
+  },
+  {
+    label: 'USA',
+    value: 'usa',
+    emoji: '🇺🇸',
+    desc: 'USA (美国)',
+  },
+  {
+    label: 'Japan',
+    value: 'japan',
+    emoji: '🇯🇵',
+    desc: 'Japan (日本)',
+  },
+  {
+    label: 'Korea',
+    value: 'korea',
+    emoji: '🇰🇷',
+    desc: 'Korea (韩国)',
+  },
+];
 
 export default function AppHeader() {
   return (
-    <Layout.Header style={headerStyle}>Header</Layout.Header>
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{
+          width: 250,
+        }}
+        placeholder="select one country"
+        defaultValue={['china']}
+        onChange={handleChange}
+        optionLabelProp="label"
+        options={options}
+        optionRender={(option) => (
+          <Space>
+            <span role="img" aria-label={option.data.label}>
+              {option.data.emoji}
+            </span>
+            {option.data.desc}
+          </Space>
+        )}
+      />
+       <Button type="primary">Add asset</Button>
+    </Layout.Header>
   )
 }
