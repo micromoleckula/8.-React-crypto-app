95029470f444e1898add181b8ce6aea2acbc80dc
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-03-11 23:50:49 +0200

    end modal

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -19,7 +19,28 @@ export default function CoinInfoModal({ coin }) {
     </Flex>
     <Divider />
     <Typography.Paragraph>
-      
+      <Typography.Text strong>1 hour: </Typography.Text>
+      <Tag color={coin.priceChange1h > 0 ? 'green' : 'red'}>{coin.priceChange1h}%</Tag>
+      <Typography.Text strong>1 day: </Typography.Text>
+      <Tag color={coin.priceChange1d > 0 ? 'green' : 'red'}>{coin.priceChange1d}%</Tag>
+      <Typography.Text strong>1 weak: </Typography.Text>
+      <Tag color={coin.priceChange1w > 0 ? 'green' : 'red'}>{coin.priceChange1w}%</Tag>
+    </Typography.Paragraph>
+    <Typography.Paragraph>
+      <Typography.Text strong>Price: </Typography.Text>
+      {coin.price.toFixed(2)}$
+    </Typography.Paragraph>
+    <Typography.Paragraph>
+      <Typography.Text strong>Price BTC: </Typography.Text>
+      {coin.priceBtc}
+    </Typography.Paragraph>
+    <Typography.Paragraph>
+      <Typography.Text strong>Market Cap: </Typography.Text>
+      {coin.marketCap}
+    </Typography.Paragraph>
+    <Typography.Paragraph>
+      <Typography.Text strong>Contract Address: </Typography.Text>
+      {coin.contractAddress}
     </Typography.Paragraph>
   </>
   )
