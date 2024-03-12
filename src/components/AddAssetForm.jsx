6f21a24eb8cef7a2d8d4b951ceff64429f3b05cb
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-03-12 23:27:23 +0200

    add select

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,3 +1,32 @@
+import {Select, Space} from 'antd'
+import { useState } from "react";
+import { useCrypto } from '../context/crypto-context';
+
 export default function AddAssetForm() {
+  const {crypto} = useCrypto()
+  const [coin, setCoin] = useState(null)
+
+  if (!coin) {
+    return (
+      <Select
+        style={{
+          width: '100%',
+        }}
+        onSelect={(v) => setCoin(crypto.find((c) => c.id == v))}
+        placeholder="Select coin"
+        options={crypto.map(coin => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img style={{width: 20}} src={option.data.icon} alt={option.data.label} />{' '}
+            {option.data.label}
+          </Space>
+        )}
+      />  
+    )
+  }
   return <form>Form</form>
 }
